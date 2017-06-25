f5da34484ffbe6c61900386a3fe5d902530ebbc7
Mario de Biase <debiasej@gmail.com>
2017-06-25 14:30:42 +0200

    Apollo client setup

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  // Every single record that comes back from the server, will have a ID.
+  // This will allow Apollo client to uniquely identify every fetched record
+  dataIdFromObject: record => record.id
+})
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        Auth Starter
+      </div>
+    </ApolloProvider>
   );
 };
 
