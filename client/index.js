cb5af445a1037d6b6538d0cb0dce22433fdcf7cb
Mario de Biase <debiasej@gmail.com>
2017-06-29 08:18:28 +0200

    Include cookies in apollo requets

--- client/index.js
+++ client/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import { Router, hashHistory, Route, IndexRoute } from 'react-router'
 
 import App from './components/App'
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+})
+
 const client = new ApolloClient({
+  // Set a custom network interface to tell apollo client that have to send the session cookie
+  networkInterface,
   // Every single record that comes back from the server, will have a ID.
   // This will allow Apollo client to uniquely identify every fetched record
   dataIdFromObject: record => record.id
